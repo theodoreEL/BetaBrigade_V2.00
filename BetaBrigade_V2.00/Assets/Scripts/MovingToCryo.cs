6bf73c18ebba9cda2c5004f8c7fb152e70bc35ab
whitmakr <35874812+whitmakr@users.noreply.github.com>
2018-02-21 14:17:54 -0600

    Move to Cryo
    MovingToCryo script
    collision trigger in hub behind locked door to go to cryo room

---
+++ BetaBrigade_V2.00/Assets/Scripts/MovingToCryo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovingToCryo : MonoBehaviour {
+
+    private GameObject player;
+   
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+   
+    IEnumerator LoadNextScene()
+    {
+        DontDestroyOnLoad(player);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("CryoRoom");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
