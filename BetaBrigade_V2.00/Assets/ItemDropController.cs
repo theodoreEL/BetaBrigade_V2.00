398949b3a471d4f85a7b9f5f22053a9ace68a516
bradleybdr <35905607+bradleybdr@users.noreply.github.com>
2018-02-26 14:19:47 -0600

    Boss Drops
    Assets Added:
    Boss Drops Controller Script

---
+++ BetaBrigade_V2.00/Assets/ItemDropController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropController : MonoBehaviour {
+
+	//GAME OBJECT Array to store the items that can be dropped
+	public GameObject[] itemDrops = new GameObject[10];
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		/* if (health is <= 0)
+			{
+				dropItem();
+			}*/
+	}
+
+	void dropItem()
+	{
+		//gernerate a random number
+		int itemNumber = Random.Range(0, 10);
+		//use the random number to select an item from the array
+		GameObject itemToDrop = itemDrops[itemNumber];
+		//create the object at the bosses location
+		Instantiate(itemToDrop, transform.position, transform.rotation);
+		//destry the boss
+		Destroy(gameObject);
+	}
+}
