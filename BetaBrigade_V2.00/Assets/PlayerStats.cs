ec2496c06b0e6f03f7ec34176922f374d07209ed
bradleybdr <35905607+bradleybdr@users.noreply.github.com>
2018-03-07 14:21:38 -0600

    Player Stats
    Assets Added:
    PlayerStats
    
    Assets Changed:
    PlayerController - added kyles player controller

---
+++ BetaBrigade_V2.00/Assets/PlayerStats.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	static int[,] heroStats = new int[6, 4];
+
+	/*
+	 * STRUCTURE OF ARRAY
+	 * X INDEX - CHARACTERS
+	 *	0 = Artist
+	 *	1 = Boombox
+	 *	2 = Segway Squid
+	 *	3 = Nura F. Gunn
+	 *	4 = Snek
+	 *	5 = hero ----------REPLACE WITH NAME ONCE KNOWN
+	 *	
+	 *	Y INDEX - STATS
+	 *	0 = speed
+	 *	1 = attack
+	 *	2 = health
+	 *	3 = defense
+	 *	
+	 */
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		//INITALIZE ALL THE STATS IN THE ARRAY
+
+		//HERO 0 STATS
+		heroStats[0, 0] = 0;
+		heroStats[0, 1] = 0;
+		heroStats[0, 2] = 0;
+		heroStats[0, 3] = 0;
+
+		//HERO 1 STATS
+		heroStats[1, 0] = 0;
+		heroStats[1, 1] = 0;
+		heroStats[1, 2] = 0;
+		heroStats[1, 3] = 0;
+
+		//HERO 2 STATS
+		heroStats[2, 0] = 0;
+		heroStats[2, 1] = 0;
+		heroStats[2, 2] = 0;
+		heroStats[2, 3] = 0;
+
+		//HERO 3 STATS
+		heroStats[3, 0] = 0;
+		heroStats[3, 1] = 0;
+		heroStats[3, 2] = 0;
+		heroStats[3, 3] = 0;
+
+		//HERO 4 STATS
+		heroStats[4, 0] = 0;
+		heroStats[4, 1] = 0;
+		heroStats[4, 2] = 0;
+		heroStats[4, 3] = 0;
+
+		//HERO 5 STATS
+		heroStats[5, 0] = 0;
+		heroStats[5, 1] = 0;
+		heroStats[5, 2] = 0;
+		heroStats[5, 3] = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+	/*
+	 * PLAYER STATS
+	 * STEPS TO FOLLOW:
+	 * 1) figure out what charcter your are
+	 * 2) find the coorisponding stats
+	 * 3) set the stats to the player.
+	 */ 
+
+	public static void statChangeTo(int character)
+	{
+		// 1) FIGURE OUT THE CHARACTER YOU ARE BECOMING - this is done by the character being passed into the function
+		int characterToBe = character;
+
+		// 2) FIND AND SET THE STATS TO THE PLAYER DEPENDING ON NEW CHARACTER
+		//process through the stats array (Y indexs) to set stats
+		
+		PlayerController.Character.speed = heroStats[characterToBe, 0];
+		PlayerController.Character.attack = heroStats[characterToBe, 1];
+		PlayerController.Character.health = heroStats[characterToBe, 2];
+		PlayerController.Character.defense = heroStats[characterToBe, 3];
+
+	}
+
+
+}
--- BetaBrigade_V2.00/Assets/Scripts/PlayerController.cs
+++ BetaBrigade_V2.00/Assets/Scripts/PlayerController.cs
@@ -1,174 +1,251 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerController: MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float moveSpeed;
-    private bool wait;
-    private float horizMoveVelocity;
-    private float vertMoveVelocity;
-    private Rigidbody2D playerRigidBody;
-    //public Transform firePosition;
-    public BulletController bullet;
-    public Transform bulletProj;
-    private float shootHoriz;
-    private float shootVert;
-    private float offset = 1.15f;
-    private Vector3 shootPos;
-    //Vector2 prevDir = Vector2.right;
+	public struct Character
+	{
+		public bool isActive;
+		public GameObject character;
+		public SpriteRenderer charSprite;
 
-    // This creates the multiple characters. The 3 we decided to start with are the artist, boombox, and segway squid
-    GameObject artist, boomBox, segway, eighty, snek;
-    int characterselect;
-    int cooldown;
+		public int characterID;
+		//BB ADDS
+		static public int speed;
+		static public int attack;
+		static public int health;
+		static public int defense;
 
-    private void Awake()
-    {
-        cooldown = 0;
-        characterselect = 1;
-        artist = GameObject.Find("artistCharacter"); // The artist character
-        boomBox = GameObject.Find("boomBoxCharacter"); // The boombox character
-        segway = GameObject.Find("segwaySquid"); // The segway character
-        eighty = GameObject.Find("nerfGun"); // The eighty's guy character
-        snek = GameObject.Find("snakeGunner"); // The snake with a gun
-    }
+		public Character(GameObject person, bool enabled, int ID)
+		{
+			character = person;
+			isActive = enabled;
+			charSprite = person.GetComponent<SpriteRenderer>();
+			characterID = ID;
 
-    // Use this for initialization
-    void Start () {
-        wait = false;
-        playerRigidBody = GetComponent<Rigidbody2D>();
-    }
+			PlayerStats.statChangeTo(characterID);
+		}
+	};
 
-    // Update is called once per frame
-    void FixedUpdate() {
-        //Movement: sets moveVelocity of horizontal and vertical movement to 0 each update, unless WASD keys are pressed, then sets them to movespeed
-        //and makes playerRigidbody velocity equal to velocity given by specific key pressed
+	private const string BACKGROUND = "Default";
+	private const string FOREGROUND = "Foreground";
+	private bool wait;
+	public float moveSpeed;
+	private float horizMoveVelocity, vertMoveVelocity, shootHoriz, shootVert;
+	private Rigidbody2D playerRigidBody;
+	public BulletController bullet;
+	private float offset = 1.15f;
+	private Vector3 shootPos;
+	public bool hasKey = false;
+	private GameObject key;
+	[HideInInspector]
+	public bool eastSideCryo = false, westSideCryo = false, southSideCryo = false;
+	private int count = 0;
+	private GameObject otherCharSpawners;
+	private SpriteRenderer parentSprite;
 
-        horizMoveVelocity = 0f;
-        vertMoveVelocity = 0f;
+	// This creates the multiple characters. The 3 we decided to start with are the artist, boombox, and segway squid
+	[HideInInspector]
+	public Character[] characters = new Character[5];
+	Character artist, boomBox, segway, eighty, snek;
+	[HideInInspector]
+	public int characterselect;
+	int cooldown;
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            horizMoveVelocity = moveSpeed;
-        }
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            horizMoveVelocity = -moveSpeed;
-        }
+		cooldown = 0;
+		characterselect = 0;
+		artist = new Character(GameObject.Find("artistCharacter"), true, 0); // The artist character
+		boomBox = new Character(GameObject.Find("boomBoxCharacter"), false, 1); // The boombox character
+		segway = new Character(GameObject.Find("segwaySquid"), false, 2); // The segway character
+		eighty = new Character(GameObject.Find("nerfGun"), false, 3); // The eighty's guy character
+		snek = new Character(GameObject.Find("snakeGunner"), false, 4); // The snake with a gun
+		characters[0] = artist;
+		characters[1] = boomBox;
+		characters[2] = segway;
+		characters[3] = eighty;
+		characters[4] = snek;
+	}
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            vertMoveVelocity = moveSpeed;
-        }
+	//OnEnable, OnDisable, and OnLevelFinishedLoading new way for unity's old OnLevelLoad function or w/e, places player at location inside cryoroom when entering it compared to the door that they entered
+	void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            vertMoveVelocity = -moveSpeed;
-        }
+	void OnDisable()
+	{
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
 
-        playerRigidBody.velocity = new Vector2(horizMoveVelocity, vertMoveVelocity);
 
-        shootHoriz = Input.GetAxisRaw("FireHoriz");
-        shootVert = Input.GetAxisRaw("FireVert");
-        bullet.direction = new Vector2(shootHoriz, shootVert);
-        shootPos = new Vector3(transform.position.x + offset * shootHoriz, transform.position.y + offset * shootVert, 0);
-        if ((shootHoriz != 0 || shootVert != 0) && !wait)
-        {
-            wait = true;
-            Instantiate(bullet, shootPos, transform.rotation);
-            Invoke("ShotBullet", .1f);
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+	{
+		for (int i = 0; i < characters.Length; i++)
+		{
+			characters[i].character.SetActive(true);
+		}
+		if (westSideCryo)
+		{
+			transform.position = GameObject.FindGameObjectWithTag("WestCryo").transform.position;
+			westSideCryo = false;
+		}
+		if (eastSideCryo)
+		{
+			transform.position = GameObject.FindGameObjectWithTag("EastCryo").transform.position;
+			eastSideCryo = false;
+		}
+		if (southSideCryo)
+		{
+			transform.position = GameObject.FindGameObjectWithTag("SouthCryo").transform.position;
+			southSideCryo = false;
+		}
 
-        }
-    }
+		otherCharSpawners = GameObject.FindGameObjectWithTag("OtherSpawnParent");
 
-    void Update()
-    {
-        // A cooldown tier for swithcing characters.
-        if (cooldown > 0)
-        {
-            cooldown--;
-        }
+		for (int i = 0; i < characters.Length; i++)
+		{
+			if (!(characters[i].isActive))
+			{
+				otherCharSpawners.transform.GetChild(count).GetComponent<SpriteRenderer>().sprite = characters[count].charSprite.sprite;
+				count++;
+			}
+		}
+		count = 0;
+	}
+	// Use this for initialization
+	void Start()
+	{
+		wait = false;
+		playerRigidBody = GetComponent<Rigidbody2D>();
+		key = GameObject.FindWithTag("Pickup");
+		parentSprite = gameObject.GetComponent<SpriteRenderer>();
+		parentSprite.sprite = characters[characterselect].charSprite.sprite;
+	}
 
-        // Increments the character number using the Jump button which is the Space Bar
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (characterselect == 1 && cooldown < 1)
-            {
-                characterselect = 2;
-                cooldown = 300;
-            }
-            else if (characterselect == 2 && cooldown < 1)
-            {
-                characterselect = 3;
-                cooldown = 300;
-            }
-            else if (characterselect == 3 && cooldown < 1)
-            {
-                characterselect = 4;
-                cooldown = 300;
-            }
-            else if (characterselect == 4 && cooldown < 1)
-            {
-                characterselect = 5;
-                cooldown = 300;
-            }
-            else if (characterselect == 5 && cooldown < 1)
-            {
-                characterselect = 1;
-                cooldown = 300;
-            }
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		//Movement: sets moveVelocity of horizontal and vertical movement to 0 each update, unless WASD keys are pressed, then sets them to movespeed
+		//and makes playerRigidbody velocity equal to velocity given by specific key pressed
 
-        }
-        // Sets the character based on the value of characterselect
-        if (characterselect == 1)
-        {
-            artist.SetActive(true);
-            snek.SetActive(false);
-            segway.SetActive(false);
-            eighty.SetActive(false);
-            boomBox.SetActive(false);
-        }
-        else if (characterselect == 2)
-        {
-            artist.SetActive(false);
-            boomBox.SetActive(true);
-            snek.SetActive(false);
-            eighty.SetActive(false);
-            segway.SetActive(false);
+		horizMoveVelocity = 0f;
+		vertMoveVelocity = 0f;
 
-        }
-        else if (characterselect == 3)
-        {
-            boomBox.SetActive(false);
-            segway.SetActive(true);
-            artist.SetActive(false);
-            eighty.SetActive(false);
-            snek.SetActive(false);
-        }
-        else if (characterselect == 4)
-        {
-            segway.SetActive(false);
-            eighty.SetActive(true);
-            artist.SetActive(false);
-            snek.SetActive(false);
-            boomBox.SetActive(false);
-        }
-        else if (characterselect == 5)
-        {
-            eighty.SetActive(false);
-            snek.SetActive(true);
-            artist.SetActive(false);
-            boomBox.SetActive(false);
-            segway.SetActive(false);
-        }
-    }
+		if (Input.GetKey(KeyCode.D))
+		{
+			horizMoveVelocity = moveSpeed;
+		}
 
-    void ShotBullet()
-    {
-        wait = false;
-    }
+		if (Input.GetKey(KeyCode.A))
+		{
+			horizMoveVelocity = -moveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			vertMoveVelocity = moveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			vertMoveVelocity = -moveSpeed;
+		}
+
+		playerRigidBody.velocity = new Vector2(horizMoveVelocity, vertMoveVelocity);
+
+		if (!hasKey) //as long as character doesn't have a key, they can shoot
+		{
+			shootHoriz = Input.GetAxisRaw("FireHoriz");
+			shootVert = Input.GetAxisRaw("FireVert");
+			bullet.direction = new Vector2(shootHoriz, shootVert);
+			shootPos = new Vector3(transform.position.x + offset * shootHoriz, transform.position.y + offset * shootVert, 0);
+			if ((shootHoriz != 0 || shootVert != 0) && !wait)
+			{
+				wait = true;
+				Instantiate(bullet, shootPos, transform.rotation);
+				Invoke("ShotBullet", .1f);
+
+			}
+		}
+	}
+
+
+	void Update()
+	{
+
+		// A cooldown tier for switching characters.
+		if (cooldown > 0)
+		{
+			cooldown--;
+		}
+
+		if (hasKey)
+		{
+			if (Input.GetKeyDown(KeyCode.Q))
+			{
+				key.transform.parent = null;
+				hasKey = false;
+			}
+
+		}
+		// Increments the character number using the Jump button which is the Space Bar
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (characterselect == 0 && cooldown < 1)
+			{
+				characterselect = 1;
+				cooldown = 300;
+			}
+			else if (characterselect == 1 && cooldown < 1)
+			{
+				characterselect = 2;
+				cooldown = 300;
+			}
+			else if (characterselect == 2 && cooldown < 1)
+			{
+				characterselect = 3;
+				cooldown = 300;
+			}
+			else if (characterselect == 3 && cooldown < 1)
+			{
+				characterselect = 4;
+				cooldown = 300;
+			}
+			else if (characterselect == 4 && cooldown < 1)
+			{
+				characterselect = 0;
+				cooldown = 300;
+			}
+		}
+
+		// Sets the character based on the value of characterselect
+		parentSprite.sprite = characters[characterselect].charSprite.sprite;
+		for (int i = 0; i < characters.Length; i++)
+		{
+			if (characterselect == i)
+			{
+				characters[i].character.SetActive(true);
+				characters[i].isActive = true;
+			}
+			else
+			{
+				characters[i].character.SetActive(false);
+				characters[i].isActive = false;
+			}
+		}
+	}
+
+	void ShotBullet()
+	{
+		wait = false;
+	}
 }
 
 
