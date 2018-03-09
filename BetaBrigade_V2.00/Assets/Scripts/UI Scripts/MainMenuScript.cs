c2589b8994661f56f4cb3b0582db9f9ff9d27153
theodoreEL <34727776+theodoreEL@users.noreply.github.com>
2018-03-09 14:22:13 -0600

    Added in game pause menu

---
+++ BetaBrigade_V2.00/Assets/Scripts/UI Scripts/MainMenuScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+
+    public Button isEasy;
+    public Button isNormal;
+    public Button isHard;
+    public Button isYes;
+    public Button isNo;
+
+    // Use this for initialization
+    void Start () {
+        isEasy.onClick.AddListener(EasyTask);
+        isNormal.onClick.AddListener(NormalTask);
+        isHard.onClick.AddListener(HardTask);
+        isYes.onClick.AddListener(YesTask);
+        isNo.onClick.AddListener(NoTask);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    void ContinueTask()
+    {
+        //Load last saved game
+    }
+    void EasyTask()
+    {
+        //Start game in Easy mode
+        SceneManager.LoadScene("Hub");
+    }
+    void NormalTask()
+    {
+        //Start game in Normal mode
+        SceneManager.LoadScene("Hub");
+    }
+    void HardTask()
+    {
+        //Start game in Hard mode
+        SceneManager.LoadScene("Hub");
+    }
+    void YesTask()
+    {
+        //Quit game
+    }
+    void NoTask()
+    {
+        isNo.animationTriggers.pressedTrigger = isNo.animationTriggers.normalTrigger;
+    }
+}
---
+++ BetaBrigade_V2.00/Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    private static bool pause;
+    public GameObject pMenu;
+    public GameObject oMenu;
+
+	// Use this for initialization
+	void Start () {
+        pause = false;
+        pMenu.SetActive(false);
+        //oMenu.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause)
+            {
+                Game();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Game()
+    {
+        Time.timeScale = 1;
+        pMenu.SetActive(false);
+        pause = false;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        pMenu.SetActive(true);
+        pause = true;
+    }
+
+    public void Options()
+    {
+        Time.timeScale = 0;
+        pMenu.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start Menu");
+    }
+}
