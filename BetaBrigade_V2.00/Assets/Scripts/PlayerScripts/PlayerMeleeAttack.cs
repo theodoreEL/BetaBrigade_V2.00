dee0e70be7e72e4df340f11e90f67a6cc5606db1
whitmakr <35874812+whitmakr@users.noreply.github.com>
2018-03-25 14:21:04 -0500

    MeleeAttack
    melee

--- BetaBrigade_V2.00/Assets/Scripts/PlayerScripts/PlayerMeleeAttack.cs
+++ BetaBrigade_V2.00/Assets/Scripts/PlayerScripts/PlayerMeleeAttack.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class PlayerMeleeAttack : MonoBehaviour {
 
-    public GameObject attack;
+    public GameObject attackHoriz;
+    public GameObject attackVert;
     public GameObject player;
+    public bool wait = false;
 
     void Update()
     {
 
-        if (Input.GetAxisRaw("FireHoriz") == 1)
+        if (Input.GetAxisRaw("FireHoriz") == 1 && !wait)
         {
-            Instantiate(attack, new Vector2(transform.position.x + 1, transform.position.y), transform.rotation);
-
+            Invoke("AttackRight", 2);
+            wait = true;
         }
-        else if (Input.GetAxisRaw("FireVert") == 1)
+        else if (Input.GetAxisRaw("FireVert") == 1 && !wait)
         {
-            Instantiate(attack, new Vector2(transform.position.x, transform.position.y + 2), transform.rotation);
 
+            Invoke("AttackUp", 2);
+            wait = true;
         }
-        else if (Input.GetAxisRaw("FireHoriz") == -1)
+        else if (Input.GetAxisRaw("FireHoriz") == -1 && !wait)
         {
-            Instantiate(attack, new Vector2(transform.position.x - 1, transform.position.y), transform.rotation);
-
+            Invoke("AttackLeft", 2);
+            wait = true;
         }
-        else if (Input.GetAxisRaw("FireVert") == -1)
+        else if (Input.GetAxisRaw("FireVert") == -1 && !wait)
         {
-            Instantiate(attack, new Vector2(transform.position.x, transform.position.y - 2), transform.rotation);
-
+            Invoke("AttackDown", 2);
+            wait = true;
         }
+    }
+
+    void AttackRight()
+    {
+        Instantiate(attackHoriz, new Vector2(transform.position.x + 1, transform.position.y), transform.rotation);
+        wait = false;
+    }
+
+    void AttackUp()
+    {
+        Instantiate(attackVert, new Vector2(transform.position.x, transform.position.y + 2), transform.rotation);
+        wait = false;
+    }
+
+    void AttackLeft()
+    {
+        Instantiate(attackHoriz, new Vector2(transform.position.x - 1, transform.position.y), transform.rotation);
+        wait = false;
+    }
+
+    void AttackDown()
+    {
+        Instantiate(attackVert, new Vector2(transform.position.x, transform.position.y - 2), transform.rotation);
+        wait = false;
     }
 }
